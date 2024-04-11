c8d5cfa63e49ca0568f2bd8ec7530ba216ed00e2
Shikawa <andurandalex@gmail.com>
2024-04-11 22:05:35 +0200

    commit add : other tries

--- public/Game/map.js
+++ public/Game/map.js
@@ -1,155 +1,104 @@
-import { Rect } from "kaboom";
 import kaboom from "./node_modules/kaboom/dist/kaboom.mjs";
 
 kaboom({
-  scale: 4,
   background: [0, 0, 0],
 });
 
-loadSprite("bricks", "./assets/bricks.png");
-loadSprite("wood", "./assets/wood.png");
+loadSprite("bricks", "./assets/bricks.png", {
+  sliceX: 3,
+});
+loadSprite("wood", "./assets/wood.png", {
+  sliceX: 2,
+});
 loadSprite("obstacle", "./assets/obstacle.png");
-loadSprite("player", "./assets/player.png");
-
-addLevel(
-  [
-    "xxxxxxxxxxxxxxx",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-    "               ",
-  ],
-  {
-    tileWidth: 50,
-    tileHeight: 50,
-    tiles: {
-      " ": () => [sprite("floor", { frame: ~~rand(0, 8) })],
+loadSprite("golem", "golem.png", {
+  sliceX: 10,
+  sliceY: 1,
+  anims: {
+    idle: {
+      from: 0,
+      to: 9,
+      speed: 8,
+      loop: true,
     },
-  }
-);
+  },
+});
 
-var wood_block = "x";
-var brick_block = "o";
+scene("main", (levelIdx) => {
+  var obstacle_block = "=";
+  var wood_block = "x";
+  var brick_block = "o";
 
-const map = addLevel(
-  [
-    "===============",
-    "=@ xxx  xx x  =",
-    "= =x=x=x=x=x= =",
-    "= xxoox xoxxx =",
-    "=x=x= =x=x=x=x=",
-    "=xoooxx xxoxxx=",
-    "= = =x=x=x=x= =",
-    "=xxxooxxxxoxxx=",
-    "=x=x=x=x=x= =x=",
-    "=xxx oxxxxxxxx=",
-    "= =x=o=x=x=x= =",
-    "=  x xx xxxx  =",
-    "===============",
-  ],
-  {
+  const SPEED = 300;
+
+  const characters = {
+    "@": {
+      sprite: "player",
+    },
+  };
+
+  const levels = [
+    [
+      "===============",
+      "=@ xxx  xx x  =",
+      "= =x=x=x=x=x= =",
+      "= xxoox xoxxx =",
+      "=x=x= =x=x=x=x=",
+      "=xoooxx xxoxxx=",
+      "= = =x=x=x=x= =",
+      "=xxxooxxxxoxxx=",
+      "=x=x=x=x=x= =x=",
+      "=xxx oxxxxxxxx=",
+      "= =x=o=x=x=x= =",
+      "=  x xx xxxx  =",
+      "===============",
+    ],
+  ];
+
+  const level = addLevel(levels[levelIdx], {
     tileWidth: 50,
     tileHeight: 50,
+    pos: vec2(50, 50),
     tiles: {
-      "=": () => [
+      obstacle_block: () => [
         sprite("obstacle"),
         area(),
         body({ isStatic: true }),
-        tile({ isObstacle: true }),
+        anchor("center"),
+        "obstacle",
       ],
       wood_block: () => [
         sprite("wood"),
         area(),
         body({ isStatic: true }),
-        tile({ isObstacle: true }),
+        anchor("center"),
+        "wood",
       ],
       brick_block: () => [
         sprite("bricks"),
         area(),
         body({ isStatic: true }),
-        tile({ isObstacle: true }),
+        anchor("center"),
+        "bricks",
       ],
+      "@": () => [sprite("golem"), area(), body(), anchor("center"), "player"],
     },
+  });
+
+  const player = level.get("player")[0];
+
+  const dirs = {
+    left: LEFT,
+    right: RIGHT,
+    up: UP,
+    down: DOWN,
+  };
+
+  for (const dir in dirs) {
+    onKeyDown(dir, () => {
+      player.move(dirs[dir].scale(SPEED));
+    });
   }
-);
-
-const player = map.spawn(
-  [
-    sprite("player", { anim: "idle" }),
-    area({ shape: new Rect(vec2(0, 6), 12, 12) }),
-    body(),
-    tile(),
-  ],
-  2,
-  2
-);
-
-map.spawn(
-  [
-    sprite("ogre"),
-    area({ scale: 0.5 }),
-    body({ isStatic: true }),
-    tile({ isObstacle: true }),
-  ],
-  5,
-  4
-);
-
-const SPEED = 120;
-
-const dirs = {
-  left: LEFT,
-  right: RIGHT,
-  up: UP,
-  down: DOWN,
-};
-
-player.onUpdate(() => {
-  camPos(player.pos);
 });
 
-onKeyDown("right", () => {
-  player.flipX = false;
-  sword.flipX = false;
-  player.move(SPEED, 0);
-  sword.pos = vec2(-4, 9);
-});
-
-onKeyDown("left", () => {
-  player.flipX = true;
-  sword.flipX = true;
-  player.move(-SPEED, 0);
-  sword.pos = vec2(4, 9);
-});
-
-onKeyDown("up", () => {
-  player.move(0, -SPEED);
-});
-
-onKeyDown("down", () => {
-  player.move(0, SPEED);
-});
-
-["left", "right", "up", "down"].forEach((key) => {
-  onKeyPress(key, () => {
-    player.play("run");
-  });
-  onKeyRelease(key, () => {
-    if (
-      !isKeyDown("left") &&
-      !isKeyDown("right") &&
-      !isKeyDown("up") &&
-      !isKeyDown("down")
-    ) {
-      player.play("idle");
-    }
-  });
-});
+go("main", 0);
