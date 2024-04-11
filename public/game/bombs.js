0fc2429475ca05d02f61bf3bda7516ba24a07e3d
alexisbaud <alexisbaud@proton.me>
2024-04-12 00:08:25 +0200

    adding explode method to Fruit class and adding kaboom for the visual

--- public/game/bombs.js
+++ public/game/bombs.js
@@ -1,27 +1,99 @@
+import kaboom from "./node_modules/kaboom/dist/kaboom.mjs";
+
+const k = kaboom({
+  scale: 4,
+  font: "monospace",
+});
+
+loadRoot(`assets/`);
+loadSprite("boom", "/explosion.png", {
+  sliceX: 7,
+  anims: {
+    "explode": {
+      from: 0,
+      to: 6,
+      speed: 10,
+      loop: false,
+    },
+  },
+});
+
+const boom = add([
+	sprite("boom"),
+	pos(-200,-200),
+	anchor("center"),
+])
+
 class Fruit {
   constructor(name, damage, radiusX, radiusY, diagRadius, renew, timer, speed) {
+    const blocksize = 20;
     this.amount = 1;
     this.name = name;
     this.damage = damage;
-    this.radiusX = radiusX;
-    this.radiusY = radiusY;
+    this.radiusX = radiusX * blocksize;
+    this.radiusY = radiusY * blocksize;
     this.diagRadius = diagRadius;
     this.renew = renew;
     this.timer = timer;
     this.speed = speed;
   }
 
-  place() {
-    this.amount -= 1;
-    // attendre la valeur timer avant que la bombe explode
-    if (diagRadius > 0) {
-      // explosion en cercle (traverse les mur)
-    } else {
-      // explosion classique
+  explode(obstacles, players, posX, posY) {
+    const blocksize = 20;
+    for (let x = posX - this.radiusX; x <= posX + this.radiusX; x = x + blocksize) {
+      for (let y = posY - this.radiusY; y <= posY + this.radiusY; y = y + blocksize) {
+        if ((x != posX &&
+          y == posY) ||
+        (y != posY &&
+          x == posX)) {
+            const boom = add ([
+              sprite("boom"),
+              pos(x,y),
+              anchor("center")
+            ])
+            boom.play("explode")
+          }
+        const player = players.find(
+          (player) => player.posX === x && player.posY === y
+        );
+        if (player) {
+          if (
+            (player.posX < posX + this.radiusX &&
+              player.posX > posX + this.radiusX &&
+              player.posY == posY) ||
+            (player.posY < posY + this.radiusY &&
+              player.posY > posY + this.radiusY &&
+              player.posX == posX)
+          ) {
+            player.hp -= 1;
+          }
+        }
+        const obstacle = obstacles.find(
+          (obstacle) => obstacle.posX === x && obstacle.posY === y
+        );
+        if (obstacle) {
+          if (
+            (obstacle.posX < posX + this.radiusX &&
+              obstacle.posX > posX + this.radiusX &&
+              obstacle.posY == posY) ||
+            (obstacle.posY < posY + this.radiusY &&
+              obstacle.posY > posY + this.radiusY &&
+              obstacle.posX == posX)
+          ) {
+            obstacle.hp -= 1;
+          }
+        }
+      }
     }
-    this.amount += 1;
+      boom.play("explode")
   }
 }
+
+const test = new Fruit("test", 1, 3, 3, 0, true, 5, 0);
+
+onMousePress(() => {
+  test.explode([],[], 100 ,100);
+});
 
 let bombs = [];
 let dbRes = [];
